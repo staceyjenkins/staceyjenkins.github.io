2dd4f5c9f591675c0a997cd0d823612ca44b3434
staceyjenkins <hello@staceyjenkins.co.uk>
2020-08-03 11:56:01 +0100

    Adding PTEngine tracking

--- assets/scripts/app/functions.js
+++ assets/scripts/app/functions.js
@@ -7,6 +7,19 @@ function loadWaypoints(){
 });   
        });
     };
+
+function loadPTEngine(){
+         window._pt_lt = new Date().getTime();
+        window._pt_sp_2 = [];
+        _pt_sp_2.push('setAccount,3428f523');
+        var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+        (function() {
+            var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+            atag.src = _protocol + 'cjs.ptengine.com/pta_en.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(atag, s);
+        })();   
+};
     
     function loadDisqus(){
         require(['disqusloader'], function(){ 
@@ -16,9 +29,8 @@ function loadWaypoints(){
 
 require(["jquery", "fontawesome"], function() {
     $(document).ready(function() {
-        $siteFunctions = [loadWaypoints(), loadDisqus()];
+        $siteFunctions = [loadWaypoints(), loadDisqus(), loadPTEngine()];
         for (var n = 0; n < $siteFunctions.length; n++) setTimeout($siteFunctions[n], 4e3);
-        console.log("Javascript loaded")
     });
 });
 
