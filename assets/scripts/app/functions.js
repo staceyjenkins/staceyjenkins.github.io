f01e2afd2656ff88b60750464eaa684962ba6450
staceyjenkins <hello@staceyjenkins.co.uk>
2020-08-03 12:26:53 +0100

    Update functions.js

--- assets/scripts/app/functions.js
+++ assets/scripts/app/functions.js
@@ -8,19 +8,6 @@ function loadWaypoints(){
        });
     };
 
-function loadPTEngine(){
-        window._pt_lt = new Date().getTime();
-        window._pt_sp_2 = [];
-        _pt_sp_2.push('setAccount,3428f523');
-        var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-        (function() {
-            var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
-            atag.src = _protocol + 'cjs.ptengine.com/pta_en.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(atag, s);
-        })();   
-};
-
 // Hotjar Tracking Code for http://www.staceyjenkins.co.uk//
 function loadHotjar(){
     (function(h,o,t,j,a,r){
