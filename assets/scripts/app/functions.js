9881d2f7ed322f319f436136b66746089e41385d
staceyjenkins <hello@staceyjenkins.co.uk>
2020-08-03 12:23:56 +0100

    Update functions.js

--- assets/scripts/app/functions.js
+++ assets/scripts/app/functions.js
@@ -20,7 +20,19 @@ function loadPTEngine(){
             s.parentNode.insertBefore(atag, s);
         })();   
 };
-    
+
+// Hotjar Tracking Code for http://www.staceyjenkins.co.uk//
+function loadHotjar(){
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:388183,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
+
     function loadDisqus(){
         require(['disqusloader'], function(){ 
         disqusLoader( '.disqus', $options );
@@ -29,7 +41,7 @@ function loadPTEngine(){
 
 require(["jquery", "fontawesome"], function() {
     $(document).ready(function() {
-        $siteFunctions = [loadWaypoints(), loadDisqus(), loadPTEngine()];
+        $siteFunctions = [loadWaypoints(), loadDisqus(), loadHotjar()];
         for (var n = 0; n < $siteFunctions.length; n++) setTimeout($siteFunctions[n], 4e3);
     });
 });
