26ad07b47d18a964e3b3a0173b756269b3222ee5
staceyjenkins <hello@staceyjenkins.co.uk>
2020-08-18 16:31:05 +0100

    Adding Heap

--- _site/assets/scripts/app/functions.js
+++ _site/assets/scripts/app/functions.js
@@ -42,9 +42,14 @@ function loadInstagram(){
         });
     };
 
+function loadHeap(){
+      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("116937336");
+};
+
 require(["jquery", "fontawesome"], function() {
     $(document).ready(function() {
-        $siteFunctions = [loadWaypoints(), loadDisqus(), loadHotjar(), loadInstagram()];
+        $siteFunctions = [loadWaypoints(), loadDisqus(), loadInstagram(), loadHotjar(), loadHeap()];
         for (var n = 0; n < $siteFunctions.length; n++) setTimeout($siteFunctions[n], 4e3);
     });
 });
