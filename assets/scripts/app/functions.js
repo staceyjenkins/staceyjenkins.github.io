1423a966f6a6a6946846839c261ad0be6dea9313
staceyjenkins <hello@staceyjenkins.co.uk>
2020-08-30 16:25:49 +0100

    Optimising smooth scrolling

--- assets/scripts/app/functions.js
+++ assets/scripts/app/functions.js
@@ -51,17 +51,42 @@ function loadHeap(){
 require(["jquery", "fontawesome"], function() {
     $(document).ready(function() {
         
-        $('a').on('click')(function(event){
-            if (this.hash !== ""){
-                event.preventDefault();
-                var hash = this.hash;
-                
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top}, 800, function(){
-                    window.location.hash;
-                });
-            }
+        // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
         });
+      }
+    }
+  });
         
         $siteFunctions = [loadWaypoints(), loadDisqus(), loadInstagram(), loadHotjar()];
         for (var n = 0; n < $siteFunctions.length; n++) setTimeout($siteFunctions[n], 4e3);
