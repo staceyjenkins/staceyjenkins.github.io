64c7e118ad5d69e061fa1a748295b34a4333bd5e
staceyjenkins <hello@staceyjenkins.co.uk>
2020-11-11 20:00:17 +0000

    Javascript Fix

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,5 +1,5 @@
 requirejs.config({
-    baseUrl:'http://www.staceyjenkins.co.uk/assets/scripts',
+    baseUrl:'https://www.staceyjenkins.co.uk/assets/scripts',
     paths:{
         disqusloader:'lib/disqusloader-min',
         jquery: '//code.jquery.com/jquery-3.5.1.min',
